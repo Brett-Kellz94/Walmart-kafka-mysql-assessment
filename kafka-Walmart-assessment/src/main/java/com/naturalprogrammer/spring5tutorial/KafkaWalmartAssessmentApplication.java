5cb5d09dcabead5ce529f05074b58353f7ad538e
Brett-Kellz94 <brett.kelley@hotmail.com>
2021-06-11 12:38:29 -0400

    first commit

---
+++ kafka-Walmart-assessment/src/main/java/com/naturalprogrammer/spring5tutorial/KafkaWalmartAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.spring5tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWalmartAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWalmartAssessmentApplication.class, args);
+	}
+
+}
